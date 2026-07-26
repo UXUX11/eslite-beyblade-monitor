a11252c0016b45a70f56d2fda4f1387f7e2cebd2
UXUX11 <john7051228@gmail.com>
2026-07-26 17:08:25 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 WEBHOOK_URL = os.environ["WEBHOOK_URL"]
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 
 KEYWORDS = [
     "BEYBLADE X",
--- notifier.py
+++ notifier.py
@@ -1,5 +1,5 @@
 import requests
-from config import WEBHOOK_URL
+from config import WEBHOOK_URL, LINE_CHANNEL_ACCESS_TOKEN
 
 
 class Notifier:
@@ -30,7 +30,35 @@ class Notifier:
             timeout=10
         )
 
+        self.send_line(message)
+
         print("Status:", response.status_code)
         print("Response:", response.text)
 
         return response.status_code == 204
+    def send_line(self, message):
+
+        headers = {
+            "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}",
+            "Content-Type": "application/json"
+        }
+
+        data = {
+            "to": "U2e156da7b872114a21c02277a46bd7a7",
+            "messages": [
+                {
+                    "type": "text",
+                    "text": message
+                }
+            ]
+        }
+
+        response = requests.post(
+            "https://api.line.me/v2/bot/message/push",
+            headers=headers,
+            json=data,
+            timeout=10
+        )
+
+        print("LINE:", response.status_code)
+        print(response.text)
